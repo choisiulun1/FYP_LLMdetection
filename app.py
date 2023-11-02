90da06b9b73b8482fd84637d86027b3d5374b399
Siulun <36674999+choisiulun1@users.noreply.github.com>
2023-11-02 15:25:22 +0800

    add requirements.txt

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/api/*": {"origins": ["http://localhost:5001", "http://127.0.0.1:5001"]}})
+
 @app.route('/api/data')
 def get_data():
     return jsonify({'data': 'Hello from Flask!'})
